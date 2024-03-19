ae08005ce3f0683c8aa84ae42f1eacfaf3bc4f46
eshansv95 <162437801+eshansv95@users.noreply.github.com>
2024-03-19 10:12:07 +0530

    Add files via upload

---
+++ myhomepage.js
@@ -0,0 +1,8 @@
+let snake=document.getElementById("snake");
+let tictactoe=document.getElementById("tictactoe");
+tictactoe.addEventListener("click",function(){
+    window.location.href="tic tac main.html";
+});
+snake.addEventListener("click",function(){
+    window.location.href="snakegamemain.html";
+});
